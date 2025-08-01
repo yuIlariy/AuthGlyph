8d2e0fc1acb4678bbe4df1396888a619ffa7ff62
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-08-01 20:15:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.client.default import DefaultBotProperties
+from config import TOKEN
+from handlers.authwatcher import register_auth_handlers
 
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(storage=MemoryStorage())
+
+register_auth_handlers(dp)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
