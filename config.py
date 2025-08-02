a353d54f7bc8633d6f05f1ec9ab4789684f6d138
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-08-02 16:43:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 TOKEN = "your_bot_token_here"
 ADMIN_ID = 123456789
 AUTH_LOG_PATH = "/var/log/auth.log"
-GEO_API = "https://ipwhois.app/json/{ip}"
+GEO_API = "https://api.ipgeolocation.io/ipgeo?apiKey=16e467004cfe436f9c1f118bc643232b&ip="
